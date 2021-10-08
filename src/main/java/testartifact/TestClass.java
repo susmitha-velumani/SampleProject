f27bea2513cf2d43f450175d82c999c13d5b4c2c
Obuli Sundar R <obuliskcet@gmail.com>
2021-10-08 22:52:46 +0530

    Commiting with Java Maven project

---
+++ src/main/java/testartifact/TestClass.java
@@ -0,0 +1,15 @@
+/**
+ * @author Obuli
+ */
+package testartifact;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I am doing training on Jenkins........");
+		System.out.println("Tetsing Poling functionality...");
+		
+	}
+
+}
